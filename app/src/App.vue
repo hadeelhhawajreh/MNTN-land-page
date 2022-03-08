a15cc8067c2727bafd99e41563ebad5e33fed56b
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2022-03-08 10:35:50 +0200

    MNTN land page

---
+++ app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <image-container />
+    <item-container />
+  </section>
+</template>
+
+<script>
+import ImageContainer from "./components/ImageContainer.vue";
+import ItemContainer from "./components/ItemContainer.vue";
+export default {
+  name: "App",
+  components: {
+    ImageContainer,
+    ItemContainer,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ app/src/components/NavigationLink.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="nav-cont">
+    <a v-for="link in links" :key="link.id"><p>{{ link.name }}</p></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationLink",
+  data() {
+    return {
+      links: [
+        { name: "Equipment", id: "Equipment" },
+        { name: "About us", id: "AboutUs" },
+        { name: "Blog", id: "Blog" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.nav-cont {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 15rem;
+}
+p {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1.125rem;
+  line-height: 1.313rem;
+  color: #ffffff;
+}
+a{
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
