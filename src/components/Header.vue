5805565f09c772e5ab0c7cba96d6ac14a1910326
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2022-03-08 12:15:09 +0200

    add drawer

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header-section">
+    <p class="text">MNTN</p>
+    <navigation-link />
+    <div class="button-container">
+      <img src="../assets/search.svg" alt="search" class="searchSVG" />
+      <button @click="openDrawer"><p class="btn-text">MENU</p></button>
+      <div class="dash"></div>
+    </div>
+    <DrawerNavigation  />
+  </div>
+</template>
+
+<script>
+import "../Styles/HeaderSection.style.css";
+import gsap from "gsap";
+import NavigationLink from "./NavigationLink.vue";
+import DrawerNavigation from "./Drawer.vue";
+export default {
+  name: "HeaderSection",
+  components: {
+    NavigationLink,
+    DrawerNavigation,
+  },
+  methods: {
+    openDrawer() {
+      this.openNavigator();
+    },
+    openNavigator() {
+      let ele = document.getElementById("drawer");
+      gsap.to(ele, {
+        width: 287,
+        duration: 0.5,
+      });
+    },
+  },
+};
+</script>
